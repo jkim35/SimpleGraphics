120d2cd01e067c43b772d0964fc501bd6113d24a
duckonesie23 <155586579+jkim35@users.noreply.github.com>
2024-12-16 14:29:34 -0500

    yayy

--- SimpleGraphics.java
+++ SimpleGraphics.java
@@ -9,14 +9,19 @@ public class SimpleGraphics {
         guy.setColor(Color.RED);
         guy.fill();
 
-        int distance;
-        while(){
+        double distance = 0;
+        int steps = 0;
+        while(distance<50){
             int vertical = (int) (Math.random()*21)-10;
             int horizontal = (int) (Math.random()*21)-10;
             guy.translate(vertical, horizontal);
+            steps++;
             try { Thread.sleep(100);}
             catch(Exception ex) {};
-            distance = guy
+            distance = Math.sqrt(Math.pow(guy.getX()-150,2)+Math.pow(guy.getY()-150,2));
+            if(distance>50){
+                System.out.println("Escaped in "+steps+" steps");
+            }
         }
     }
 }
